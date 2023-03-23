6adde7abb9fab6032583689366a0425a32f07a8d
Lalo Freak <lalo.gwerh@gmail.com>
2023-03-23 18:34:14 -0300

    first commit

---
+++ myapp/functions/my_functions.py
@@ -0,0 +1,88 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import io
+import base64
+
+def standardize(X):
+  X_mean = np.mean(X, axis=0)
+  X_std = np.std(X, axis=0)
+  X_std[X_std == 0] = 1
+  X_standardized = (X - X_mean) / X_std
+  return X_standardized
+
+def standardized_betas(X, y):
+  X_standardized = standardize(X)
+  y_mean = np.mean(y)
+  y_std = np.std(y)
+  y_standardized = (y - y_mean) / y_std
+  beta_standardized = np.linalg.inv(X_standardized.T.dot(X_standardized)).dot(X_standardized.T).dot(y_standardized)
+  return beta_standardized
+
+def r_squared(X, y):
+  y_pred = my_linear_model(X, y)
+  y_mean = np.mean(y)
+  ss_tot = np.sum((y - y_mean)**2)
+  ss_res = np.sum((y - y_pred)**2)
+  r2 = 1 - (ss_res / ss_tot)
+  return r2
+
+def my_linear_model(X, y):
+  X_T = np.transpose(X)
+  beta = np.linalg.inv(X_T.dot(X)).dot(X_T).dot(y)
+  
+  y_pred = X.dot(beta)
+  
+  return y_pred
+
+def my_pred(X, Y, beta_standardized):
+  beta0 = np.mean(Y) - np.sum(beta_standardized[i]*np.mean(X.iloc[:,i]) for i in range(X.shape[1]))
+  _pred = beta0
+  for i in range(len(beta_standardized)):
+    _pred += beta_standardized[i]*X.iloc[:,i]
+  return _pred
+
+
+def my_data(X, Y):
+  fig, ax = plt.subplots()
+  for i in X:
+    ax.scatter(X[i], Y, label=i)
+  ax.set_xlabel('Variables independientes (X)')
+  ax.set_ylabel('Variable dependiente (Y)')
+  ax.set_title('Datos Excel')
+  ax.legend()
+
+  buffer = io.BytesIO()
+  plt.savefig(buffer, format='png')
+  buffer.seek(0)
+
+  image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8').replace('\n', '')
+
+  image_url = f"data:image/png;base64,{image_base64}"
+
+  return image_url
+
+def my_plot(Y, reg_pred, r2, beta_standardized):
+  fig, ax = plt.subplots()
+  ax.scatter(Y, reg_pred, label='Predicciones')
+  ax.plot([Y.min(), Y.max()], [Y.min(), Y.max()], 'k--', lw=2, label='Regresión Lineal')
+  ax.set_xlabel('Valores observados')
+  ax.set_ylabel('Valores predichos')
+  ax.set_title('Regresión Lineal Múltiple')
+  ax.legend()
+
+  textstr = '\n'.join((
+    r'$R^2=%.3f$' % (r2),
+    r'$\beta_{standardized}=$' + str(beta_standardized)))
+  props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+  ax.text(0.20, 0.15, textstr, transform=ax.transAxes, fontsize=8,
+    verticalalignment='top', bbox=props)
+
+  buffer = io.BytesIO()
+  plt.savefig(buffer, format='png')
+  buffer.seek(0)
+
+  image_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8').replace('\n', '')
+
+  image_url = f"data:image/png;base64,{image_base64}"
+
+  return image_url
---
+++ myapp/views.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+from django.shortcuts import redirect, render
+import myapp.functions.my_functions as l_reg
+import myapp.misc.globals as file
+
+def index(request):
+    archivos = os.listdir("myapp/data")
+    if request.method == "POST":
+        ruta = request.POST.get("ruta")
+        file.file_path = "myapp/data/" + ruta
+        return redirect("home")
+    return render(request, "index.html", {"archivos": archivos})
+
+def home(request):
+    if request.method == "POST":
+        file.Y = request.POST.get("Y").strip()
+        file.X = request.POST.get("X").split(",")
+
+        return redirect("predict")
+    df = pd.read_excel(file.file_path)
+    return render(request, 'home.html', {'data': df.to_html()})
+
+def predict(request):
+    df = pd.read_excel(file.file_path)
+
+    X = df[file.X]
+    Y = df[file.Y]
+
+    beta_standardized = l_reg.standardized_betas(X, Y)
+
+    reg_pred = l_reg.my_pred(X, Y, beta_standardized)
+    
+    r2 = l_reg.r_squared(X, Y)
+
+    data = l_reg.my_data(X, Y)
+    plot = l_reg.my_plot(Y, reg_pred, r2, beta_standardized)
+
+    return render(request, 'predict.html', {'r2': r2, 'beta_standardized': beta_standardized, 'data': data, 'plot': plot})
