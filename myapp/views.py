1e59628edee3ea0b9f04679739718f8eb6ac3cec
Lalo Freak <lalo.gwerh@gmail.com>
2023-03-23 19:54:41 -0300

    estilos

--- myapp/views.py
+++ myapp/views.py
@@ -22,18 +22,24 @@ def home(request):
     return render(request, 'home.html', {'data': df.to_html()})
 
 def predict(request):
-    df = pd.read_excel(file.file_path)
+    if file.X != '' and file.Y != '':
+        df = pd.read_excel(file.file_path)
 
-    X = df[file.X]
-    Y = df[file.Y]
+        X = df[file.X]
+        Y = df[file.Y]
 
-    beta_standardized = l_reg.standardized_betas(X, Y)
+        beta_standardized = l_reg.standardized_betas(X, Y)
 
-    reg_pred = l_reg.my_pred(X, Y, beta_standardized)
-    
-    r2 = l_reg.r_squared(X, Y)
+        reg_pred = l_reg.my_pred(X, Y, beta_standardized)
+        
+        r2 = l_reg.r_squared(X, Y)
 
-    data = l_reg.my_data(X, Y)
-    plot = l_reg.my_plot(Y, reg_pred, r2, beta_standardized)
+        data = l_reg.my_data(X, Y)
+        plot = l_reg.my_plot(Y, reg_pred, r2, beta_standardized)
+    else:
+        r2 = "error"
+        beta_standardized = "error"
+        data = ""
+        plot = "error"
 
     return render(request, 'predict.html', {'r2': r2, 'beta_standardized': beta_standardized, 'data': data, 'plot': plot})
