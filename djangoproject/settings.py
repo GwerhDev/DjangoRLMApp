33c40a2ee997afef8a5fdd7db30fb58766019b9b
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-28 19:35:47 -0400

    prepared to deployment

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -134,6 +134,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICS_DIRS = (os.path.join(BASE_DIR, 'myapp/static'),)
 
 # Default primary key field type
