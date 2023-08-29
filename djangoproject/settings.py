87f0dadb7d7ec07f15b1dce9912ffc860672d9eb
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-28 21:46:03 -0400

    prepared to deployment

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -132,11 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/admin/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICS_DIRS = (os.path.join(BASE_DIR, 'myapp/static'),)
+STATICS_DIRS = (os.path.join(BASE_DIR, 'myapp/static/'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
