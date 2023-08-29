949b69465ac86fa0c57b50deca6d5e5b59b763e4
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-28 20:51:53 -0400

    prepared to deployment

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/admin'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'public')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICS_DIRS = (os.path.join(BASE_DIR, 'myapp/static'),)
 
