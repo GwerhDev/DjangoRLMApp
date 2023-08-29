d86a719298bfeacefef8b8c9cd6e93c3f811b171
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-28 21:55:57 -0400

    prepared to deployment

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'myapp',
     'myapp.functions',
     'myapp.misc',
+    'myapp.data'
 ]
 
 MIDDLEWARE = [
@@ -132,11 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 
-STATICS_DIRS = (os.path.join(BASE_DIR, 'myapp/static/'),)
+STATICS_DIRS = os.path.join(BASE_DIR, '/myapp/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
