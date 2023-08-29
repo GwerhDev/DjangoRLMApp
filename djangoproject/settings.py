49a08c2caea6972bef249b1dd06d5f3a214f2d00
Lalo Freak <lalofreak.dev@gmail.com>
2023-08-29 10:06:33 -0400

    prepared to deployment

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'es-ch'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -136,11 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICS_DIRS = os.path.join(BASE_DIR, '/myapp/static/')
+STATICS_DIRS = os.path.join(BASE_DIR, 'myapp/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
